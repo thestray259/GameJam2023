0ee11f1ffc2b89e7428893e52a2bfbf8efab17c2
Tobie Allen <thestray259@gmail.com>
2023-08-19 17:02:36 -0600

    base clicks, got adaptive performance to turn off

--- GameJam2023/Assets/Scripts/UIButtonClicks.cs
+++ GameJam2023/Assets/Scripts/UIButtonClicks.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIButtonClicks : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] TMP_Text moneyText;
+    [SerializeField] Button button1;
+    [SerializeField] Button button2;
+    [SerializeField] Button button3;
+    [SerializeField] Button button4;
+    [SerializeField] Button settingsButton;
+    [SerializeField] Button settingsBackButton;
+
+    private Economy economy;
+
+    public void UpdateMoney(double money, TMP_Text moneyText)
     {
-        
+        moneyText.text = money.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnButton1Click()
     {
-        
+        Debug.Log("Button1 clicked");
+    }
+
+    public void OnButton2Click()
+    {
+        Debug.Log("Button2 clicked");
+    }
+
+    public void OnButton3Click()
+    {
+        Debug.Log("Button3 clicked");
+    }
+
+    public void OnButton4Click()
+    {
+        Debug.Log("Button4 clicked");
+    }
+
+    public void OnUpgrade()
+    {
+        Debug.Log("Upgrade Clicked");
+    }
+
+    public void OnSettings()
+    {
+        Debug.Log("Settings Clicked");
     }
 }
