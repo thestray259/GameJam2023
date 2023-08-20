231e688faf0baf7bbd2696e9fbd76d4bf30dc81a
Tobie Allen <thestray259@gmail.com>
2023-08-19 18:02:27 -0600

    settings opens and closes, part-way through upgrade button

--- GameJam2023/Assets/Scripts/UIButtonClicks.cs
+++ GameJam2023/Assets/Scripts/UIButtonClicks.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -7,6 +8,11 @@ using UnityEngine.UI;
 public class UIButtonClicks : MonoBehaviour
 {
     [SerializeField] TMP_Text moneyText;
+    [SerializeField] TMP_Text amountGained1;
+    [SerializeField] TMP_Text amountGained2;
+    [SerializeField] TMP_Text amountGained3;
+    [SerializeField] TMP_Text amountGained4;
+
     [SerializeField] Button button1;
     [SerializeField] Button button2;
     [SerializeField] Button button3;
@@ -14,7 +20,9 @@ public class UIButtonClicks : MonoBehaviour
     [SerializeField] Button settingsButton;
     [SerializeField] Button settingsBackButton;
 
-    private Economy economy;
+    [SerializeField] GameObject settingsMenu;
+
+    [SerializeField] Economy economy;
 
     public void UpdateMoney(double money, TMP_Text moneyText)
     {
@@ -24,6 +32,9 @@ public class UIButtonClicks : MonoBehaviour
     public void OnButton1Click()
     {
         Debug.Log("Button1 clicked");
+
+        economy.money += GetAmount(amountGained1.text);
+        UpdateMoney(economy.money, moneyText);
     }
 
     public void OnButton2Click()
@@ -44,10 +55,59 @@ public class UIButtonClicks : MonoBehaviour
     public void OnUpgrade()
     {
         Debug.Log("Upgrade Clicked");
+        // get current button clicked
+        // get AMOUNT in children
+        // GetAmount()
+        // compare to money, if money >= amount 
+        // do the upgrade, subtract amount from money and update it
+        // else do nothing
+
+        var thisButton = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject;
+
+        var amountObject = GetChildWithName(thisButton, "AMOUNT");
+        string s = amountObject.GetComponent<TMP_Text>().text;
+        var amountNum = GetAmount(s);
+
+        if (economy.money >= amountNum)
+        {
+            economy.money -= amountNum;
+            UpdateMoney(economy.money, moneyText);
+        }
     }
 
     public void OnSettings()
     {
         Debug.Log("Settings Clicked");
+
+        settingsMenu.SetActive(true);
     }
+
+    public void OnSettingsBack()
+    {
+        settingsMenu.SetActive(false);
+    }
+
+    // HELPERS
+    public double GetAmount(string amountGained)
+    {
+        string amount = amountGained.Substring(1);        
+
+        return Convert.ToDouble(amount);
+    }
+
+    GameObject GetChildWithName(GameObject obj, string name)
+    {
+        Transform trans = obj.transform;
+        Transform childTrans = trans.Find(name);
+        if (childTrans != null)
+        {
+            return childTrans.gameObject;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+
 }
